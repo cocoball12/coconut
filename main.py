c94ca61ba7aa244434b47f63918ed89b67e622a6
cocoball12 <dkdldbfj90@gmail.com>
2025-07-18 23:23:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -335,7 +335,7 @@ async def on_member_join(member):
             await welcome_channel.send(embed=embed, view=view)
 
             # 추가 안내문을 별도 메시지로 전송
-            additional_message = "심심해서 들어온거면 관리진들이 불러줄 때 빨리 답장하고 부르면 음챗방 오셈\n답도 안하고 활동 안할거면 **걍 딴 서버나 가라** 그런 새끼 받아주는 서버 아님 <@&ㅇㄹㅇㄹ>"
+            additional_message = "심심해서 들어온거면 관리진들이 불러줄 때 빨리 답장하고 부르면 음챗방 오셈\n답도 안하고 활동 안할거면 **걍 딴 서버나 가라** 그런 새끼 받아주는 서버 아님 @ㅇㄹㅇㄹ"
             await welcome_channel.send(additional_message)
 
             await asyncio.sleep(5)
